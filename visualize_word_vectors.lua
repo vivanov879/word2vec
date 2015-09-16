c00850b6938bfde1e263695473bacb7dc865d757
Владимир Иванов <vivanov879@ya.ru>
2015-09-16 12:13:15 +0300

    no message

--- visualize_word_vectors.lua
+++ visualize_word_vectors.lua
@@ -9,7 +9,7 @@ nngraph.setDebug(true)
 
 
 inv_vocabulary_en = table.load('inv_vocabulary_en')
-
+vocabulary_en = table.load('vocabulary_en')
 
 visualize_words = {"the", "a", "an", ",", ".", "?", "!", "``", "''", "--", "good", "great", "cool", "brilliant", "wonderful", "well", "amazing", "worth", "sweet", "enjoyable", "boring", "bad", "waste", "dumb", "annoying"}
 visualize_indexes = {}
@@ -22,7 +22,15 @@ end
 
 m = torch.load('model')
 
+word_center = torch.Tensor(#vocabulary_en)
+for i = 1, #vocabulary_en do 
+  word_center[i] = i
+end
+word_outer = word_center:clone()
 
+_, x_outer, x_center = unpack(m:forward({word_center, word_outer}))
+
+x = x_outer + x_center
 
 mean = x:mean(1)
 std = x:std(1)
@@ -33,7 +41,9 @@ std_expanded = torch.expand(std, x:size(1), x:size(2))
 x = x:add(-mean_expanded)
 x = x:cdiv(std_expanded)
 
-u,s,v = torch.svd(x)
+covariance = torch.mm(x:t(), x)
+
+u,s,v = torch.svd(covariance)
 
 
 
