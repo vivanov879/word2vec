31f0229b7342cfac9937a05477bebc2ad584b08d
Владимир Иванов <vivanov879@ya.ru>
2015-09-17 09:03:27 +0300

    no message

--- sentiment_analysis.lua
+++ sentiment_analysis.lua
@@ -7,6 +7,11 @@ local model_utils=require 'model_utils'
 require 'table_utils'
 nngraph.setDebug(true)
 
+
+inv_vocabulary_en = table.load('inv_vocabulary_en')
+vocabulary_en = table.load('vocabulary_en')
+
+
 phrases_filtered_tensor, sentiment_lables_filtered_tensor, phrases_filtered_text = unpack(torch.load('sentiment_features_and_labels'))
 
 batch_size = 10000
@@ -23,10 +28,16 @@ function gen_batch()
   
   features = phrases_filtered_tensor[{{data_index, data_index + batch_size}, {}}]
   labels = sentiment_lables_filtered_tensor[{{data_index, data_index + batch_size}, {}}]
+  text_first_sentence = phrases_filtered_text[data_index]
+  text_first_sentence_readable = {}
+  for i, word in pairs(text_first_sentence) do 
+    text_first_sentence_readable[#text_first_sentence_readable + 1] = vocabulary_en[word]
+  end
+  text_first_sentence_readable = table.concat(text_first_sentence_readable, ' ')
       
   data_index = data_index + 1
   
-  return features, labels
+  return features, labels, text_first_sentence_readable
 end
 
 
@@ -53,7 +64,7 @@ function feval(x_arg)
     
     local loss = 0
     
-    features, labels = gen_batch()
+    features, labels, text_first_sentence_readable = gen_batch()
             
     ------------------- forward pass -------------------
     prediction = m:forward(features)
@@ -81,7 +92,10 @@ for i = 1, 1000000 do
 
   local _, loss = optim.adagrad(feval, params, optim_state)
   if i % 1000 == 0 then
+    print(text_first_sentence_readable,prediction[1], labels[1])
     print(loss)
+    
+    
   end
   
 end
