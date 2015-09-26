6a598548199ed84529f6d94055c2fc00cb7142a4
Владимир Иванов <vivanov879@ya.ru>
2015-09-26 07:33:18 +0300

    no message

--- sentiment_analysis.lua
+++ sentiment_analysis.lua
@@ -43,15 +43,16 @@ end
 inv_vocabulary_en = table.load('inv_vocabulary_en')
 vocabulary_en = table.load('vocabulary_en')
 
+features_train, labels_train, text_train = unpack(torch.load('sentiment_train.t7'))
+phrases_dev, labels_dev, text_dev= unpack(torch.load('sentiment_dev.t7'))
 
-phrases_filtered_tensor, sentiment_lables_filtered_tensor, phrases_filtered_text = unpack(torch.load('sentiment_train.t7'))
 
-assert (phrases_filtered_tensor:size(1) == sentiment_lables_filtered_tensor:size(1))
-assert (phrases_filtered_tensor:size(1) == #phrases_filtered_text)
+assert (features_train:size(1) == labels_train:size(1))
+assert (features_train:size(1) == #text_train)
 
 batch_size = 3000
 data_index = 1
-n_data = phrases_filtered_tensor:size(1)
+n_data = features_train:size(1)
 
 function gen_batch()
   start_index = data_index
@@ -62,23 +63,23 @@ function gen_batch()
     data_index = data_index + batch_size
   end
     
-  features = phrases_filtered_tensor[{{start_index, end_index}, {}}]
-  labels = sentiment_lables_filtered_tensor[{{start_index, end_index}}]
+  features = features_train[{{start_index, end_index}, {}}]
+  labels = labels_train[{{start_index, end_index}}]
   
-  text_first_sentence = phrases_filtered_text[start_index]
-  text_first_sentence_readable = {}
-  for i, word in pairs(text_first_sentence) do 
-    text_first_sentence_readable[#text_first_sentence_readable + 1] = vocabulary_en[word]
+  text = text_train[start_index]
+  text_readable = {}
+  for i, word in pairs(text) do 
+    text_readable[#text_readable + 1] = vocabulary_en[word]
   end
-  text_first_sentence_readable = table.concat(text_first_sentence_readable, ' ')
+  text_readable = table.concat(text_readable, ' ')
  
-  return features, labels, text_first_sentence_readable
+  return features, labels, text_readable
 
 end
 
 
 x_raw = nn.Identity()()
-x = nn.Linear(phrases_filtered_tensor:size(2), 200)(x_raw)
+x = nn.Linear(features_train:size(2), 200)(x_raw)
 x = nn.Tanh()(x)
 x = nn.Linear(200, 5)(x)
 x = nn.LogSoftMax()(x)
@@ -100,7 +101,7 @@ function feval(x_arg)
     
     local loss = 0
     
-    features, labels, text_first_sentence_readable = gen_batch()
+    features, labels, text_readable = gen_batch()
             
     ------------------- forward pass -------------------
     prediction = m:forward(features)
@@ -129,7 +130,16 @@ for i = 1, 1000000 do
     local loss_train = loss[1]
     local _, predicted_class  = prediction:max(2)
     local f1_score_train, precision_train, recall_train = unpack(calc_f1(predicted_class, torch.reshape(labels, predicted_class:size(1), predicted_class:size(2))))
+    
+    local features = features_dev[{{}, {}}]
+    local labels = labels_dev[{{}}]
+    local prediction, h = unpack(m:forward(features))
+    local _, predicted_class  = prediction:max(2)
+    local loss_dev = criterion:forward(prediction, labels)
+    local f1_score_dev, precision_dev, recall_dev = unpack(calc_f1(predicted_class, torch.reshape(labels, predicted_class:size(1), predicted_class:size(2))))
+    
     print(string.format("train set: loss = %6.8f, f1_score = %6.8f, precision = %6.8f, recall = %6.8f, grad_params:norm() = %6.4e", loss_train, f1_score_train, precision_train, recall_train, grad_params:norm()))
+    print(string.format("dev set:   loss = %6.8f, f1_score = %6.8f, precision = %6.8f, recall = %6.8f", loss_dev, f1_score_dev, precision_dev, recall_dev))
 
     
   end
